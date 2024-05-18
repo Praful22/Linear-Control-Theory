f40cbe7e383a92d91d227dee4b7188c763c66e68
Praful Sigdel <Prafulsigdel5@gmail.com>
2024-05-18 13:14:22 -0700

    Added a matlab file to cover major topics and projects related to linear control theory.

---
+++ ClassicalLinearControlTechniques.m
@@ -0,0 +1,21 @@
+%% Praful Sigdel
+% May 2024
+% Matlab Code implementing Linear Control Techniques using Control System
+% Toolbox
+% Credits: Modern Control Systems, Richard C. Dorf, Robert H. Bishop
+
+%% Table of Contents
+% 1. Modeling of Physical systems in Control System Design Process
+% 2. State Variable Models in Simulink
+% 3. Feedback Control System Characteristics
+% 4. Performance of Feedback Control Systems
+% 5. Stability of Linear Feedback Systems
+% 6. Root Locus Method
+% 7. Frequence Response method
+% 8. Stability in the Frequency Domain
+% 9. Design of Feedback Control Systems
+% 10. Design of State Variable Feedback Systems
+% 11. Robust Control Systems
+% 12. Digital Control Systems
+
+
